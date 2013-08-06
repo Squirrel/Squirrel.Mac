7d0e7642a333e239ea98b62f851ae1cfa6bf7f43
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-08-06 13:22:26 -0700

    Create app target for testing updates

---
+++ TestApplication/SQRLAppDelegate.m
@@ -0,0 +1,18 @@
+//
+//  SQRLAppDelegate.m
+//  TestApplication
+//
+//  Created by Justin Spahr-Summers on 2013-08-06.
+//  Copyright (c) 2013 GitHub. All rights reserved.
+//
+
+#import "SQRLAppDelegate.h"
+
+@implementation SQRLAppDelegate
+
+- (void)applicationDidFinishLaunching:(NSNotification *)aNotification
+{
+	// Insert code here to initialize your application
+}
+
+@end
