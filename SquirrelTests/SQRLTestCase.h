bf72c8590e19c12da4db5bfbb9eb44679b5f0570
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-08-06 00:54:41 -0700

    Add zipped version of GHfM as a fixture

---
+++ SquirrelTests/SQRLTestCase.h
@@ -0,0 +1,26 @@
+//
+//  SQRLTestCase.h
+//  Squirrel
+//
+//  Created by Justin Spahr-Summers on 2013-08-06.
+//  Copyright (c) 2013 GitHub. All rights reserved.
+//
+
+@interface SQRLTestCase : SPTSenTestCase
+
+// A URL to a temporary directory tests can use.
+//
+// This directory will be deleted between each spec.
+@property (nonatomic, copy, readonly) NSURL *temporaryDirectoryURL;
+
+// The URL to the test app fixture, automatically unzipping it if necessary.
+//
+// This URL will be deleted between each spec.
+@property (nonatomic, copy, readonly) NSURL *testAppURL;
+
+// The URL to the zipped test app fixture.
+//
+// This URL will be deleted between each spec.
+@property (nonatomic, copy, readonly) NSURL *zippedTestAppURL;
+
+@end
---
+++ SquirrelTests/SQRLTestCase.m
@@ -0,0 +1,116 @@
+//
+//  SQRLTestCase.m
+//  Squirrel
+//
+//  Created by Justin Spahr-Summers on 2013-08-06.
+//  Copyright (c) 2013 GitHub. All rights reserved.
+//
+
+#import "SQRLTestCase.h"
+
+// The name (without any extension) of the test application fixture.
+static NSString * const SQRLTestCaseTestAppFixtureName = @"GitHub.app";
+
+@interface SQRLTestCase ()
+
+// The URL to the temporary directory which contains `temporaryDirectoryURL` and
+// all copied fixtures.
+@property (nonatomic, copy, readonly) NSURL *baseTemporaryDirectoryURL;
+
+@end
+
+@implementation SQRLTestCase
+
+#pragma mark Properties
+
+@synthesize baseTemporaryDirectoryURL = _baseTemporaryDirectoryURL;
+
+#pragma mark Lifecycle
+
+- (void)SPT_tearDown {
+	[self cleanUp];
+}
+
+- (void)tearDown {
+	[super tearDown];
+	[self cleanUp];	
+}
+
+- (void)cleanUp {
+	[NSFileManager.defaultManager removeItemAtURL:_baseTemporaryDirectoryURL error:NULL];
+	_baseTemporaryDirectoryURL = nil;
+}
+
+#pragma mark Temporary Directory
+
+- (NSURL *)baseTemporaryDirectoryURL {
+	if (_baseTemporaryDirectoryURL == nil) {
+		NSURL *globalTemporaryDirectory = [NSURL fileURLWithPath:NSTemporaryDirectory() isDirectory:YES];
+		_baseTemporaryDirectoryURL = [globalTemporaryDirectory URLByAppendingPathComponent:[NSProcessInfo.processInfo globallyUniqueString]];
+		
+		NSFileManager *fileManager = [[NSFileManager alloc] init];
+		NSError *error = nil;
+		BOOL success = [fileManager createDirectoryAtURL:_baseTemporaryDirectoryURL withIntermediateDirectories:YES attributes:nil error:&error];
+		STAssertTrue(success, @"Couldn't create temporary directory at %@: %@", _baseTemporaryDirectoryURL, error);
+	}
+
+	return _baseTemporaryDirectoryURL;
+}
+
+- (NSURL *)temporaryDirectoryURL {
+	return [self.baseTemporaryDirectoryURL URLByAppendingPathComponent:@"spec-data"];
+}
+
+#pragma mark Fixtures
+
+- (void)unzipMember:(NSString *)memberName fromArchiveAtURL:(NSURL *)zipURL intoDirectory:(NSURL *)destinationDirectory {
+	NSParameterAssert(memberName != nil);
+	NSParameterAssert(zipURL != nil);
+	NSParameterAssert(destinationDirectory != nil);
+
+	NSTask *task = [[NSTask alloc] init];
+	task.launchPath = @"/usr/bin/unzip";
+	task.arguments = @[ @"-qq", @"-d", destinationDirectory.path, zipURL.path, [memberName stringByAppendingString:@"*"], @"-x", @"*/.DS_Store" ];
+	[task launch];
+	[task waitUntilExit];
+	
+	BOOL success = (task.terminationStatus == 0);
+	STAssertTrue(success, @"Couldn't unzip member \"%@\" from %@ into %@", memberName, zipURL, destinationDirectory);
+	STAssertTrue([NSFileManager.defaultManager fileExistsAtPath:[destinationDirectory URLByAppendingPathComponent:memberName].path], @"Member \"%@\" from %@ does not exist in %@ after unzipping", memberName, zipURL, destinationDirectory);
+}
+
+- (NSURL *)fixturesURL {
+	NSURL *fixturesURL = [self.baseTemporaryDirectoryURL URLByAppendingPathComponent:@"fixtures"];
+	if (![NSFileManager.defaultManager fileExistsAtPath:fixturesURL.path]) {
+		NSError *error = nil;
+		BOOL success = [NSFileManager.defaultManager createDirectoryAtURL:fixturesURL withIntermediateDirectories:YES attributes:nil error:&error];
+		STAssertTrue(success, @"Couldn't create fixtures directory at %@: %@", fixturesURL, error);
+	}
+
+	return fixturesURL;
+}
+
+- (NSURL *)testAppURL {
+	NSURL *testAppURL = [self.fixturesURL URLByAppendingPathComponent:SQRLTestCaseTestAppFixtureName];
+	if (![NSFileManager.defaultManager fileExistsAtPath:testAppURL.path]) {
+		NSURL *zippedURL = [[NSBundle bundleForClass:self.class] URLForResource:SQRLTestCaseTestAppFixtureName withExtension:@"zip" subdirectory:@"Fixtures"];
+		[self unzipMember:SQRLTestCaseTestAppFixtureName fromArchiveAtURL:zippedURL intoDirectory:self.fixturesURL];
+	}
+
+	return testAppURL;
+}
+
+- (NSURL *)zippedTestAppURL {
+	NSURL *testAppURL = [self.fixturesURL URLByAppendingPathComponent:@"testApp.zip"];
+	if (![NSFileManager.defaultManager fileExistsAtPath:testAppURL.path]) {
+		NSURL *bundledURL = [[NSBundle bundleForClass:self.class] URLForResource:SQRLTestCaseTestAppFixtureName withExtension:@"zip" subdirectory:@"Fixtures"];
+		
+		NSError *error = nil;
+		BOOL success = [NSFileManager.defaultManager copyItemAtURL:bundledURL toURL:testAppURL error:&error];
+		STAssertTrue(success, @"Couldn't copy %@ to %@: %@", bundledURL, testAppURL, error);
+	}
+	
+	return testAppURL;
+}
+
+@end
