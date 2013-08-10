0e14afb544edbe56f583b0b1522acf8e4e73fccc
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-08-10 01:17:51 -0700

    Test relaunching with SQRLInstaller

--- SquirrelTests/SQRLInstallerSpec.m
+++ SquirrelTests/SQRLInstallerSpec.m
@@ -38,4 +38,34 @@ it(@"should install an update", ^{
 	expect(self.testApplicationBundle.infoDictionary[SQRLBundleShortVersionStringKey]).to.equal(SQRLTestApplicationUpdatedShortVersionString);
 });
 
+it(@"should install an update and relaunch", ^{
+	xpc_object_t message = xpc_dictionary_create(NULL, NULL, 0);
+	xpc_dictionary_set_string(message, SQRLShipItCommandKey, SQRLShipItInstallCommand);
+
+	xpc_dictionary_set_string(message, SQRLTargetBundleURLKey, self.testApplicationURL.absoluteString.UTF8String);
+	xpc_dictionary_set_string(message, SQRLUpdateBundleURLKey, updateURL.absoluteString.UTF8String);
+	xpc_dictionary_set_string(message, SQRLBackupURLKey, self.temporaryDirectoryURL.absoluteString.UTF8String);
+	xpc_dictionary_set_bool(message, SQRLShouldRelaunchKey, true);
+
+	__block BOOL installed = NO;
+
+	NSString *bundleIdentifier = @"com.github.Squirrel.TestApplication";
+	NSArray *apps = [NSRunningApplication runningApplicationsWithBundleIdentifier:bundleIdentifier];
+	expect(apps.count).to.equal(0);
+
+	xpc_connection_send_message_with_reply(shipitConnection, message, dispatch_get_main_queue(), ^(xpc_object_t event) {
+		expect(xpc_dictionary_get_bool(event, SQRLShipItSuccessKey)).to.beTruthy();
+		expect([self errorFromObject:event]).to.beNil();
+
+		installed = YES;
+	});
+
+	expect(installed).will.beTruthy();
+	expect(self.testApplicationBundle.infoDictionary[SQRLBundleShortVersionStringKey]).to.equal(SQRLTestApplicationUpdatedShortVersionString);
+
+	apps = [NSRunningApplication runningApplicationsWithBundleIdentifier:bundleIdentifier];
+	expect(apps.count).to.equal(1);
+	expect([apps.lastObject bundleURL]).to.equal(self.testApplicationURL);
+});
+
 SpecEnd
