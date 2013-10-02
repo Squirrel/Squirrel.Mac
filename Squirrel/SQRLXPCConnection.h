d32b1712e40f51c578509ae7f04abd7a367f608c
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-10-02 02:44:33 -0700

    Add SQRLXPCConnection wrapper class

---
+++ Squirrel/SQRLXPCConnection.h
@@ -0,0 +1,60 @@
+//
+//  SQRLXPCConnection.h
+//  Squirrel
+//
+//  Created by Justin Spahr-Summers on 2013-10-02.
+//  Copyright (c) 2013 GitHub. All rights reserved.
+//
+
+#import "SQRLXPCObject.h"
+
+// The domain for errors originating within XPC.
+extern NSString * const SQRLXPCErrorDomain;
+
+// An unknown XPC error.
+extern const NSInteger SQRLXPCErrorUnknown;
+
+// The remote service exited.
+extern const NSInteger SQRLXPCErrorConnectionInterrupted;
+
+// The named service could not be found.
+extern const NSInteger SQRLXPCErrorConnectionInvalid;
+
+// The program will be exiting soon.
+extern const NSInteger SQRLXPCErrorTerminationImminent;
+
+@class RACSignal;
+
+// A boxed and RACified `xpc_connection_t`.
+@interface SQRLXPCConnection : SQRLXPCObject
+
+// Sends received events as `SQRLXPCObject`s.
+//
+// This signal will error if the XPC connection receives an error, or complete
+// if the connection is canceled.
+@property (nonatomic, strong, readonly) RACSignal *events;
+
+// Initializes the receiver.
+//
+// This will take over the connection's event handler.
+//
+// connection - The connection to wrap. This may be nil.
+- (id)initWithXPCObject:(xpc_connection_t)connection;
+
+// Cancels the connection.
+//
+// This will send completed upon `events`.
+- (void)cancel;
+
+// Resumes the connection from a suspended state.
+- (void)resume;
+
+// Sends the given message across the connection and listens for a reply.
+//
+// message - The message to send across the connection. This must not be nil.
+//
+// Returns a signal which will send any reply as a `SQRLXPCObject` then
+// complete on a background thread.
+- (RACSignal *)sendMessageExpectingReply:(SQRLXPCObject *)message;
+
+@end
---
+++ Squirrel/SQRLXPCConnection.m
@@ -0,0 +1,118 @@
+//
+//  SQRLXPCConnection.m
+//  Squirrel
+//
+//  Created by Justin Spahr-Summers on 2013-10-02.
+//  Copyright (c) 2013 GitHub. All rights reserved.
+//
+
+#import "SQRLXPCConnection.h"
+#import <ReactiveCocoa/EXTScope.h>
+#import <ReactiveCocoa/ReactiveCocoa.h>
+
+NSString * const SQRLXPCErrorDomain = @"SQRLXPCErrorDomain";
+const NSInteger SQRLXPCErrorUnknown = 0;
+const NSInteger SQRLXPCErrorConnectionInterrupted = 1;
+const NSInteger SQRLXPCErrorConnectionInvalid = 2;
+const NSInteger SQRLXPCErrorTerminationImminent = 3;
+
+@interface SQRLXPCConnection () {
+	RACSubject *_events;
+}
+
+@end
+
+@implementation SQRLXPCConnection
+
+#pragma mark Lifecycle
+
+- (id)initWithXPCObject:(xpc_connection_t)connection {
+	self = [super initWithXPCObject:connection];
+	if (self == nil) return nil;
+
+	_events = [[RACReplaySubject replaySubjectWithCapacity:1] setNameWithFormat:@"%@ -events", self];
+
+	if (connection != NULL) {
+		xpc_connection_set_event_handler(connection, ^(xpc_object_t event) {
+			// Intentionally introduce a retain cycle with `self`.
+			[self sendEvent:event toSubscriber:_events];
+
+			if (xpc_get_type(event) == XPC_TYPE_ERROR) {
+				[self cancel];
+
+				// When the connection finishes, break the retain cycle.
+				xpc_connection_set_event_handler(connection, ^(xpc_object_t event) {
+				});
+			}
+		});
+	}
+
+	return self;
+}
+
+- (void)dealloc {
+	[_events sendCompleted];
+}
+
+- (void)cancel {
+	[_events sendCompleted];
+
+	if (self.object != NULL) xpc_connection_cancel(self.object);
+}
+
+- (void)resume {
+	if (self.object != NULL) xpc_connection_resume(self.object);
+}
+
+#pragma mark Communication
+
+- (RACSignal *)sendMessageExpectingReply:(SQRLXPCObject *)message {
+	if (self.object == NULL) return [RACSignal empty];
+
+	return [[[RACSignal
+		createSignal:^ RACDisposable * (id<RACSubscriber> subscriber) {
+			xpc_connection_send_message_with_reply(self.object, message.object, dispatch_get_global_queue(DISPATCH_QUEUE_PRIORITY_DEFAULT, 0), ^(xpc_object_t event) {
+				[self sendEvent:event toSubscriber:subscriber];
+				[subscriber sendCompleted];
+			});
+
+			return nil;
+		}]
+		replay]
+		setNameWithFormat:@"%@ -sendMessageExpectingReply: %@", self, message];
+}
+
+- (void)sendEvent:(xpc_object_t)event toSubscriber:(id<RACSubscriber>)subscriber {
+	if (xpc_get_type(event) == XPC_TYPE_ERROR) {
+		[subscriber sendError:[self errorFromXPCError:event]];
+	} else {
+		SQRLXPCObject *wrappedEvent = [[SQRLXPCObject alloc] initWithXPCObject:event];
+		[subscriber sendNext:wrappedEvent];
+	}
+}
+
+#pragma mark Error Handling
+
+- (NSError *)errorFromXPCError:(xpc_object_t)errorEvent {
+	NSParameterAssert(xpc_get_type(errorEvent) == XPC_TYPE_ERROR);
+
+	NSInteger code;
+	if (errorEvent == XPC_ERROR_CONNECTION_INTERRUPTED) {
+		code = SQRLXPCErrorConnectionInterrupted;
+	} else if (errorEvent == XPC_ERROR_CONNECTION_INVALID) {
+		code = SQRLXPCErrorConnectionInvalid;
+	} else if (errorEvent == XPC_ERROR_TERMINATION_IMMINENT) {
+		code = SQRLXPCErrorTerminationImminent;
+	} else {
+		code = SQRLXPCErrorUnknown;
+	}
+
+	const char *description = xpc_dictionary_get_string(errorEvent, XPC_ERROR_KEY_DESCRIPTION);
+
+	NSDictionary *userInfo = nil;
+	if (description != NULL) userInfo = @{ NSLocalizedDescriptionKey: @(description) };
+
+	return [NSError errorWithDomain:SQRLXPCErrorDomain code:code userInfo:userInfo];
+}
+
+@end
