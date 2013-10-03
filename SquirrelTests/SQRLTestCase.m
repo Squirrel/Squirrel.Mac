8afae59338b481fcec4bcff094b4cd9acd650804
Justin Spahr-Summers <Justin.SpahrSummers@gmail.com>
2013-10-03 02:04:02 -0700

    Yet another ShipIt logging path for Janky

--- SquirrelTests/SQRLTestCase.m
+++ SquirrelTests/SQRLTestCase.m
@@ -63,17 +63,31 @@ static void SQRLSignalHandler(int sig) {
 	NSURL *appSupportURL = [NSFileManager.defaultManager URLForDirectory:NSApplicationSupportDirectory inDomain:NSUserDomainMask appropriateForURL:nil create:YES error:NULL];
 	NSAssert(appSupportURL != nil, @"Could not find Application Support folder");
 
-	NSURL *stdoutTestApp = [appSupportURL URLByAppendingPathComponent:@"com.github.Squirrel.TestApplication.ShipIt/ShipIt_stdout.log"];
-	NSURL *stderrTestApp = [appSupportURL URLByAppendingPathComponent:@"com.github.Squirrel.TestApplication.ShipIt/ShipIt_stderr.log"];
-	NSURL *stdoutOCUnit = [appSupportURL URLByAppendingPathComponent:@"otest.ShipIt/ShipIt_stdout.log"];
-	NSURL *stderrOCUnit = [appSupportURL URLByAppendingPathComponent:@"otest.ShipIt/ShipIt_stderr.log"];
+	NSArray *folders = @[
+		@"com.github.Squirrel.TestApplication.ShipIt",
+		@"otest.ShipIt",
+		@"otest-x86_64.ShipIt",
+	];
 
-	[[NSData data] writeToURL:stdoutTestApp atomically:YES];
-	[[NSData data] writeToURL:stderrTestApp atomically:YES];
-	[[NSData data] writeToURL:stdoutOCUnit atomically:YES];
-	[[NSData data] writeToURL:stderrOCUnit atomically:YES];
+	RACSequence *URLs = [folders.rac_sequence flattenMap:^(NSString *folder) {
+		NSURL *baseURL = [appSupportURL URLByAppendingPathComponent:folder];
+		return @[
+			[baseURL URLByAppendingPathComponent:@"ShipIt_stdout.log"],
+			[baseURL URLByAppendingPathComponent:@"ShipIt_stderr.log"]
+		].rac_sequence;
+	}];
 
-	NSArray *args = @[ @"-f", stdoutTestApp.path, stderrTestApp.path, stdoutOCUnit.path, stderrOCUnit.path ];
+	for (NSURL *URL in URLs) {
+		[[NSData data] writeToURL:URL atomically:YES];
+	}
+
+	NSArray *args = [[[URLs
+		map:^(NSURL *URL) {
+			return URL.path;
+		}]
+		startWith:@"-f"]
+		array];
+
 	NSTask *readShipIt = [NSTask launchedTaskWithLaunchPath:@"/usr/bin/tail" arguments:args];
 	NSAssert([readShipIt isRunning], @"Could not start task %@ with arguments: %@", readShipIt, args);
 
