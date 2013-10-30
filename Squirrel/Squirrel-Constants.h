e8c0e4748e48e1a7ecc22139d13e68f4506dbcd3
Keith Duncan <keith.duncan@github.com>
2013-10-30 12:38:11 +0000

    Add Squirrel-Constants for SQRLErrorDomain

---
+++ Squirrel/Squirrel-Constants.h
@@ -0,0 +1,11 @@
+//
+//  Squirrel-Constants.h
+//  Squirrel
+//
+//  Created by Keith Duncan on 30/10/2013.
+//  Copyright (c) 2013 GitHub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+extern NSString * const SQRLErrorDomain;
---
+++ Squirrel/Squirrel-Constants.m
@@ -0,0 +1,11 @@
+//
+//  Squirrel-Constants.m
+//  Squirrel
+//
+//  Created by Keith Duncan on 30/10/2013.
+//  Copyright (c) 2013 GitHub. All rights reserved.
+//
+
+#import "Squirrel-Constants.h"
+
+NSString * const SQRLErrorDomain = @"SQRLErrorDomain";
