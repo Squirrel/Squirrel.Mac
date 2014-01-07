2b33ac168c6e252e441f9b0a547d2bf18c153984
Keith Duncan <keith.duncan@github.com>
2014-01-07 22:11:47 +0000

    Update docs to reflect that the root prefs aren’t opposed to shipit state
    ShipIt state is now in the prefs too.

--- Squirrel/SQRLInstaller+Private.h
+++ Squirrel/SQRLInstaller+Private.h
@@ -11,8 +11,8 @@
 // A preferences key for the URL where the target bundle has been moved before
 // installation.
 //
-// This is stored in preferences, rather than `SQRLShipItState`, to prevent an
-// attacker from rewriting the URL during the installation process.
+// This is stored in preferences to prevent an attacker from rewriting the URL
+// during the installation process.
 //
 // Note that this key must remain backwards compatible, so ShipIt doesn't fail
 // confusingly on a newer version.
@@ -21,8 +21,8 @@ extern NSString * const SQRLInstallerOwnedTargetBundleURLKey;
 // A preferences key for the URL where the update bundle has been moved before
 // installation.
 //
-// This is stored in preferences, rather than `SQRLShipItState`, to prevent an
-// attacker from rewriting the URL during the installation process.
+// This is stored in preferences to prevent an attacker from rewriting the URL
+// during the installation process.
 //
 // Note that this key must remain backwards compatible, so ShipIt doesn't fail
 // confusingly on a newer version.
@@ -31,8 +31,8 @@ extern NSString * const SQRLInstallerOwnedUpdateBundleURLKey;
 // A preferences key for the code signature that the update _and_ target bundles
 // must match in order to be valid.
 //
-// This is stored in preferences, rather than `SQRLShipItState`, to prevent an
-// attacker from spoofing the validity requirements.
+// This is stored in preferences to prevent an attacker from spoofing the
+// validity requirements.
 //
 // Note that this key must remain backwards compatible, so ShipIt doesn't fail
 // confusingly on a newer version.
