c1410d93b67b18c49c2f4709c9f7a7d95830c05f
Keith Duncan <keith.duncan@github.com>
2014-01-07 22:12:15 +0000

    whitespace--

--- Squirrel/SQRLInstaller.m
+++ Squirrel/SQRLInstaller.m
@@ -31,7 +31,6 @@ const NSInteger SQRLInstallerErrorInvalidState = -6;
 const NSInteger SQRLInstallerErrorMovingAcrossVolumes = -7;
 const NSInteger SQRLInstallerErrorChangingPermissions = -8;
 
-
 NSString * const SQRLInstallerOwnedTargetBundleURLKey = @"OwnedTargetBundleURL";
 NSString * const SQRLInstallerOwnedUpdateBundleURLKey = @"OwnedUpdateBundleURL";
 NSString * const SQRLInstallerCodeSignatureKey = @"CodeSignature";
