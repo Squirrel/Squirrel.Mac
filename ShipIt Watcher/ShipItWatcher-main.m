14892783cd60c5dc6a45b4b5e6c64bfc0d075476
Keith Duncan <keith.duncan@github.com>
2014-01-15 10:25:42 -0800

    Start a new target for the watcher

---
+++ ShipIt Watcher/ShipItWatcher-main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  ShipIt Watcher
+//
+//  Created by Keith Duncan on 15/01/2014.
+//  Copyright (c) 2014 GitHub. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+	@autoreleasepool {
+	    
+	    // insert code here...
+	    NSLog(@"Hello, World!");
+	    
+	}
+    return 0;
+}
+
