bfa660b5bc5722c097dc4234793c718e2add04c4
Keith Duncan <keith.duncan@github.com>
2014-02-14 19:03:00 +0000

    Add docs on the multiprocess topology to `SQRLShipItConnection`

--- Squirrel/SQRLShipItConnection.h
+++ Squirrel/SQRLShipItConnection.h
@@ -17,7 +17,41 @@ extern NSString * const SQRLShipItConnectionErrorDomain;
 // The ShipIt service could not be started.
 extern const NSInteger SQRLShipItConnectionErrorCouldNotStartService;
 
-// Responsible for launching the ShipIt service to actually install an update.
+// Installing an update requires the coordination of multiple processes,
+// `SQRLShipItConnection` is responsible for submitting the launchd jobs
+// required to perform an installation request.
+//
+// The multiprocess approach to waiting for termination and installing is
+// designed to keep uses of AppKit API (i.e. `NSRunningApplication`) out of the
+// installer process - which may be running in the root bootstrap context - and
+// put them in a process running in the user bootstrap context. These processes
+// can then communicate using the file system as a message bus.
+//
+// Frustratingly although LaunchServices is daemon and root safe since 10.5, the
+// LaunchServices API for querying the running applications isn't public
+// (internally `NSRunningApplication` uses this LaunchServices private API,
+// though of course this is subject to change and `NSRunningApplication` could
+// also do other non-root safe things).
+//
+// When a user application running Squirrel wants to install an update, Squirrel
+// submits two launchd jobs, one which will wait for application termination and
+// write an empty file to the given location when the criteria are met, and the
+// other which will wait for that file to appear and then perform the install as
+// before. The "wait for termination" job is always submitted to the user
+// domain, but the installer job is submitted to a domain based on whether the
+// install location is writable by the current user.
+//
+// To perform the relaunch when the install is complete, it is safe to use the
+// LaunchServices API from the installer process, the app will be launched in
+// the user's GUI session.
+//
+// From <https://developer.apple.com/library/mac/technotes/tn2083/>
+//
+// > If the EUID of the calling process is zero, the application is launched in
+// > the context of the currently active GUI login session. If there is no
+// > currently active GUI login session (no one is logged in, or a logged in
+// > user has fast user switched to the login window), the behavior is
+// > unspecified (r. 5321293).
 @interface SQRLShipItConnection : NSObject
 
 // Returns the label for the ShipIt launchd job.
