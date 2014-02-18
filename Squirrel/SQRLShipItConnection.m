24d9ef1f8663585f25faa7428727d02b83daf675
Keith Duncan <keith.duncan@github.com>
2014-02-18 13:51:52 +0000

    Remove the debug launchd option

--- Squirrel/SQRLShipItConnection.m
+++ Squirrel/SQRLShipItConnection.m
@@ -95,10 +95,6 @@ const NSInteger SQRLShipItConnectionErrorCouldNotStartService = 1;
 
 			jobDict[@(LAUNCH_JOBKEY_STANDARDOUTPATH)] = [appSupportURL URLByAppendingPathComponent:@"ShipIt_stdout.log"].path;
 			jobDict[@(LAUNCH_JOBKEY_STANDARDERRORPATH)] = [appSupportURL URLByAppendingPathComponent:@"ShipIt_stderr.log"].path;
-
-			#if DEBUG
-			jobDict[@(LAUNCH_JOBKEY_DEBUG)] = @YES;
-			#endif
 			
 			return jobDict;
 		}]
