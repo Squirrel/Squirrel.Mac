8431e9e5e66c5b1d97fc54dc0516731843beed61
Antoine Duchateau <ad@taktik.be>
2015-12-07 13:18:49 +0100

    Fixed formatting and added documentation

--- Squirrel/NSURLConnection+RACStreamSupport.h
+++ Squirrel/NSURLConnection+RACStreamSupport.h
@@ -12,5 +12,17 @@
 @class RACSignal;
 
 @interface NSURLConnection (RACStreamSupport)
+
+/**
+ Lazily loads data for the given request in the background and save the content on the disk in a file created at the provided URL.
+ Returns a signal which will begin loading the request upon each subscription,
+ then send a `RACTuple` of the received `NSURLResponse` and destination file
+ `NSURL`. The NSURLConnection is scheduled on the main RunLoop. If any errors occur, the
+ returned signal will error out.
+
+ @param request The URL request to load. This must not be nil.
+ @param destination The file URL to the filesystem destination of the downloaded data.
+
+ */
 + (RACSignal *)rac_startAsynchronousRequest:(NSURLRequest *)request into:(NSURL*)destination;
 @end
--- Squirrel/NSURLConnection+RACStreamSupport.m
+++ Squirrel/NSURLConnection+RACStreamSupport.m
@@ -8,89 +8,89 @@
 
 #import "NSURLConnection+RACStreamSupport.h"
 
-@interface RACNSURLConnectionDataDelegate : NSObject<NSURLConnectionDataDelegate>
-@property (strong, readwrite) NSString * fileName;
-@property (strong, readwrite) NSFileHandle * file;
-@property (strong, readwrite) NSURLResponse * response;
-@property (strong, readwrite) void(^handler)(NSURLResponse* __nullable response, NSString * __nullable fileName, NSError* __nullable connectionError);
+@interface RACNSURLConnectionDataDelegate : NSObject <NSURLConnectionDataDelegate>
+@property(strong, readwrite) NSString *fileName;
+@property(strong, readwrite) NSFileHandle *file;
+@property(strong, readwrite) NSURLResponse *response;
+@property(strong, readwrite) void(^handler)(NSURLResponse *__nullable response, NSString *__nullable fileName, NSError *__nullable connectionError);
 
-- (void) cleanup;
+- (void)cleanup;
 @end
 
 @implementation RACNSURLConnectionDataDelegate
-- (instancetype) initWithFileName:(NSString *)fileName completionHandler:(void (^)(NSURLResponse* __nullable response, NSString * __nullable fileName, NSError* __nullable connectionError)) handler {
-    if (self = [super init]) {
-        self.fileName = fileName;
-        self.handler = handler;
-    }
-    return self;
+- (instancetype)initWithFileName:(NSString *)fileName completionHandler:(void (^)(NSURLResponse *__nullable response, NSString *__nullable fileName, NSError *__nullable connectionError))handler {
+	self = [super init];
+	if (self == nil) return nil;
+
+	self.fileName = fileName;
+	self.handler = handler;
+
+	return self;
 }
 
-- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse*)response {
-    self.response = response;
-    
-    if (response.suggestedFilename) {
-        self.fileName = [self.fileName.stringByDeletingLastPathComponent stringByAppendingPathComponent:response.suggestedFilename];
-    }
-    
-    [[NSFileManager defaultManager] createFileAtPath:self.fileName contents:nil attributes:nil];
-    self.file = [NSFileHandle fileHandleForUpdatingAtPath:self.fileName];
+- (void)connection:(NSURLConnection *)connection didReceiveResponse:(NSURLResponse *)response {
+	self.response = response;
+
+	if (response.suggestedFilename) {
+		self.fileName = [self.fileName.stringByDeletingLastPathComponent stringByAppendingPathComponent:response.suggestedFilename];
+	}
+
+	[[NSFileManager defaultManager] createFileAtPath:self.fileName contents:nil attributes:nil];
+	self.file = [NSFileHandle fileHandleForUpdatingAtPath:self.fileName];
 }
 
 - (void)connection:(NSURLConnection *)connection didReceiveData:(NSData *)data {
-    if (self.file)  {
-        [self.file seekToEndOfFile];
-    }
-    NSLog(@"Received %lu bytes",(unsigned long)data.length);
-    [self.file writeData:data];
+	if (self.file) {
+		[self.file seekToEndOfFile];
+	}
+	NSLog(@"Received %lu bytes", (unsigned long) data.length);
+	[self.file writeData:data];
 }
 
-- (void)connectionDidFinishLoading:(NSURLConnection*)connection { 
-    [self.file closeFile];
-    self.file = nil;
+- (void)connectionDidFinishLoading:(NSURLConnection *)connection {
+	[self.file closeFile];
+	self.file = nil;
 
-    self.handler(self.response, self.fileName, nil);
+	self.handler(self.response, self.fileName, nil);
 }
 
 - (void)connection:(NSURLConnection *)connection didFailWithError:(NSError *)error {
-    self.handler(nil, nil, error);
+	self.handler(nil, nil, error);
 }
 
-- (void) cleanup {}
+- (void)cleanup {
+}
 @end
 
 @implementation NSURLConnection (RACStreamSupport)
 
-+ (RACSignal *)rac_startAsynchronousRequest:(NSURLRequest *)request into:(NSURL*)destination {
-    NSCParameterAssert(request != nil);
-    
-    return [[RACSignal
-             createSignal:^(id<RACSubscriber> subscriber) {
-                 RACNSURLConnectionDataDelegate * delegate;
-                 delegate = [[RACNSURLConnectionDataDelegate alloc] initWithFileName:destination.path completionHandler:^(NSURLResponse *response, NSString * fileName, NSError *error) {
-                     [delegate cleanup];
-                     
-                     if (response == nil || error) {
-                         [subscriber sendError:error];
-                     } else {
-                         [subscriber sendNext:RACTuplePack(response, [NSURL fileURLWithPath:fileName])];
-                         [subscriber sendCompleted];
-                     }
-                 }];
-                 
-                 NSURLConnection * connection = [[NSURLConnection alloc]
-                                                 initWithRequest:request
-                                                 delegate:delegate startImmediately:NO];
-                 
-                 [connection scheduleInRunLoop:[NSRunLoop mainRunLoop]
-                                       forMode:NSDefaultRunLoopMode];
-                 [connection start];
-                 
-                 return [RACDisposable disposableWithBlock:^{
-                     [connection cancel];
-                 }];
-             }]
-            setNameWithFormat:@"+rac_startAsynchronousRequest: %@", request];
++ (RACSignal *)rac_startAsynchronousRequest:(NSURLRequest *)request into:(NSURL *)destination {
+	NSCParameterAssert(request != nil);
+
+	return [[RACSignal
+			createSignal:^(id <RACSubscriber> subscriber) {
+				RACNSURLConnectionDataDelegate *delegate;
+				delegate = [[RACNSURLConnectionDataDelegate alloc] initWithFileName:destination.path completionHandler:^(NSURLResponse *response, NSString *fileName, NSError *error) {
+					[delegate cleanup];
+
+					if (response == nil || error) {
+						[subscriber sendError:error];
+					} else {
+						[subscriber sendNext:RACTuplePack(response, [NSURL fileURLWithPath:fileName])];
+						[subscriber sendCompleted];
+					}
+				}];
+
+				NSURLConnection *connection = [[NSURLConnection alloc] initWithRequest:request delegate:delegate startImmediately:NO];
+
+				[connection scheduleInRunLoop:[NSRunLoop mainRunLoop] forMode:NSDefaultRunLoopMode];
+				[connection start];
+
+				return [RACDisposable disposableWithBlock:^{
+					[connection cancel];
+				}];
+			}]
+			setNameWithFormat:@"+rac_startAsynchronousRequest: %@", request];
 }
 
 @end
