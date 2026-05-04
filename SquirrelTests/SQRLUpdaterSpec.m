Squirrel/Squirrel.Mac

commit c80f0066498e72e1983fba40dc8b55bfe1ad5d15

keep existing signatures for initWithUpdateRequest

Squirrel/SQRLUpdater.m
@@ -146,13 +146,19 @@ static NSString * const SQRLUpdaterUniqueTemporaryDirectoryPrefix = @"update.";
 - (id)initWithUpdateRequest:(NSURLRequest *)updateRequest {
 	return [self initWithUpdateRequest:updateRequest requestForDownload:^(NSURL *downloadURL) {
 		return [NSURLRequest requestWithURL:downloadURL];
-	} forVersion:@"" useMode:RELEASESERVER];
+	}];
 }
 
 - (id)initWithUpdateRequest:(NSURLRequest *)updateRequest forVersion: (NSString*) version {
 	return [self initWithUpdateRequest:updateRequest requestForDownload:^(NSURL *downloadURL) {
 		return [NSURLRequest requestWithURL:downloadURL];
 	} forVersion:version useMode:JSONFILE];
+}
+
+- (id)initWithUpdateRequest:(NSURLRequest *)updateRequest requestForDownload:(SQRLRequestForDownload)requestForDownload {
+	return [self initWithUpdateRequest:updateRequest requestForDownload:^(NSURL *downloadURL) {
+		return [NSURLRequest requestWithURL:downloadURL];
+	} forVersion:@"" useMode:RELEASESERVER];
 }
 
 - (id)initWithUpdateRequest:(NSURLRequest *)updateRequest requestForDownload:(SQRLRequestForDownload)requestForDownload

Squirrel/SQRLUpdater.h
@@ -152,6 +152,20 @@ typedef enum {
 //                      updateRequest param.
 //
 // Returns the initialized `SQRLUpdater`.
+- (id)initWithUpdateRequest:(NSURLRequest *)updateRequest requestForDownload:(SQRLRequestForDownload)requestForDownload;
+
+// Initializes an updater that will send the given request to check for updates
+// and passes a block to provide requests for the update downloads.
+//
+// updateRequest - Same as with initWithUpdateRequest
+// requestForDownload - Once the update url is found for the update download, allow
+//                      providing custom requests that can be costomized as desired.
+//                      Useful for including `Authorization` headers just like the
+//                      updateRequest param.
+// forVersion         - currently running version
+// useMode            - either RELEASESERVER or JSONFILE
+//
+// Returns the initialized `SQRLUpdater`.
 - (id)initWithUpdateRequest:(NSURLRequest *)updateRequest requestForDownload:(SQRLRequestForDownload)requestForDownload forVersion:(NSString*) version useMode:(SQRLUpdaterMode) mode;
 
 // Executes `checkForUpdatesCommand` (if enabled) every `interval` seconds.

commit 31dbc38e187b5df2dce59791e75e9293bc288bba

fix: forward requestForDownload in -initWithUpdateRequest:requestForDownload:
The two-arg convenience initializer accepted a requestForDownload block
and discarded it, forwarding a fresh default block to the designated
initializer instead. SQRLUpdater.h:152-157 documents this overload as
the way to attach Authorization headers to the zip download, and
h:106-107 promises the property's default is the argument passed here;
neither was true.

Regressed in c80f0066. The 4-arg designated initializer and the
writable requestForDownload property both work, which is likely why
this was never reported.

Squirrel/SQRLUpdater.m
@@ -172,9 +172,7 @@ BOOL isVersionStandard(NSString* version) {
 }
 
 - (id)initWithUpdateRequest:(NSURLRequest *)updateRequest requestForDownload:(SQRLRequestForDownload)requestForDownload {
-	return [self initWithUpdateRequest:updateRequest requestForDownload:^(NSURL *downloadURL) {
-		return [NSURLRequest requestWithURL:downloadURL];
-	} forVersion:nil useMode:RELEASESERVER];
+	return [self initWithUpdateRequest:updateRequest requestForDownload:requestForDownload forVersion:nil useMode:RELEASESERVER];
 }
 
 - (id)initWithUpdateRequest:(NSURLRequest *)updateRequest requestForDownload:(SQRLRequestForDownload)requestForDownload

SquirrelTests/SQRLUpdaterSpec.m
@@ -761,6 +761,22 @@ describe(@"isVersionStandard", ^{
 	});
 });
 
+describe(@"-initWithUpdateRequest:requestForDownload:", ^{
+	it(@"should use the provided requestForDownload block", ^{
+		NSURLRequest *updateRequest = [NSURLRequest requestWithURL:[NSURL URLWithString:@"http://fake/update-check"]];
+		SQRLUpdater *updater = [[SQRLUpdater alloc] initWithUpdateRequest:updateRequest requestForDownload:^(NSURL *downloadURL) {
+			NSMutableURLRequest *request = [NSMutableURLRequest requestWithURL:downloadURL];
+			[request setValue:@"Bearer test-token" forHTTPHeaderField:@"Authorization"];
+			return request;
+		}];
+
+		NSURL *downloadURL = [NSURL URLWithString:@"http://fake/app.zip"];
+		NSURLRequest *downloadRequest = updater.requestForDownload(downloadURL);
+		expect(downloadRequest.URL).to(equal(downloadURL));
+		expect([downloadRequest valueForHTTPHeaderField:@"Authorization"]).to(equal(@"Bearer test-token"));
+	});
+});
+
 describe(@"+isVersionAllowedForUpdate:from:", ^{
 	it(@"should compare version numbers correctly", ^{
 		expect(@([SQRLUpdater isVersionAllowedForUpdate:@"2.0.0" from:@"1.0.0"])).to(beTruthy());